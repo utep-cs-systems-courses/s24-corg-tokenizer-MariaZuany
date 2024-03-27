3855476d633189e16321debee68a7c6d0314c82c
MariaZuany <mfzuany@miners.utep.edu>
2024-03-27 17:32:17 -0600

    1 finished

--- src/history.h
+++ src/history.h
@@ -12,13 +12,31 @@ typedef struct s_List {
 } List;
 
 /* Initialize the linked list to keep the history. */
-List* init_history();
+List* init_history(){
+  List *new_list =(List*)malloc(sizeof(List));//memory allocation
+  if (new_list != NULL){
+    new_list->root = NULL; //Empty list
+    return new_list;
+  }
+  printf("Error allocating memory for new list\n");
+  return NULL;
+}
 
 /* Add a history item to the end of the list.
    List* list - the linked list
    char* str - the string to store
 */
-void add_history(List *list, char *str);
+void add_history(List *list, char *str){
+  Node *new_node = (Node*)malloc(sizeof(Node));//create new node
+
+  if(new_node == NULL){
+    printf("Error allocating memory for a new node\n");
+    return NULL;
+  }
+
+  new_node->next=NULL;
+  
+}
 
 /* Retrieve the string stored in the node where Item->id == id.
    List* list - the linked list
