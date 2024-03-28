3cf3c440876212bd7e3a5b46d717162f9ede6c0f
MariaZuany <mfzuany@miners.utep.edu>
2024-03-27 18:08:26 -0600

    casi finished

--- src/history.h
+++ src/history.h
@@ -67,7 +67,14 @@ char *get_history(List *list, int id){
 }
 
 /*Print the entire contents of the list. */
-void print_history(List *list);
+void print_history(List *list){
+  Item *curr = list->root;
+
+  while(curr!= NULL){
+    printf("ID: %d | Content: %s\n", curr->id,curr->str);
+    curr=curr->next;
+  }
+}
 
 /*Free the history list and the strings it references. */
 void free_history(List *list);
