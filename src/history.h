422b3e7907b52506f8716245467d3bb5251e9ad0
MariaZuany <mfzuany@miners.utep.edu>
2024-03-30 14:00:49 -0600

    deleted wrong code

--- src/history.h
+++ src/history.h
@@ -12,83 +12,23 @@ typedef struct s_List {
 } List;
 
 /* Initialize the linked list to keep the history. */
-List* init_history(){
-  List *new_list =(List*)malloc(sizeof(List));//memory allocation
-  if (new_list != NULL){
-    new_list->root = NULL; //Empty list
-    return new_list;
-  }
-  printf("Error allocating memory for new list\n");
-  return NULL;
-}
+List* init_history();
 
 /* Add a history item to the end of the list.
    List* list - the linked list
    char* str - the string to store
 */
-void add_history(List *list, char *str){
-  Item *new_node = (Item*)malloc(sizeof(Item));//create new node
-
-  if(new_node == NULL){
-    printf("Error allocating memory for a new node\n");
-    return;
-  }
-
-  new_node->next=NULL;
-  new_node->id = 0; //index 0
-  new_node->str = str;
-
-  if (list->root==NULL){//if the head list is empty
-    list->root = new_node;//create a node
-  }
-
-  //traverse the list
-  Item *curr = list->root; //current node is the head
-  while (curr->next != NULL){//if the next of the current is not null we move
-    curr = curr->next;//move the pointer curr to the next node
-  }
-  curr->next = new_node;
-}
+void add_history(List *list, char *str);
 
 /* Retrieve the string stored in the node where Item->id == id.
    List* list - the linked list
    int id - the id of the Item to find */
-char *get_history(List *list, int id){
-  Item *curr = list->root;//beginning of the list
-
-  while (curr != NULL){
-    if (curr->id == id){
-      return curr->str;
-    }
-    curr=curr->next;
-  }
-  //No nodes with the indicated ID
-  return NULL;
-}
+char *get_history(List *list, int id);
 
 /*Print the entire contents of the list. */
-void print_history(List *list){
-  Item *curr = list->root;
-
-  while(curr!= NULL){
-    printf("ID: %d | Content: %s\n", curr->id,curr->str);
-    curr=curr->next;
-  }
-}
+void print_history(List *list);
 
 /*Free the history list and the strings it references. */
-void free_history(List *list){
-  Item *curr = list->root;
-  Item *next;
-
-  //traverse current list
-  while(curr != NULL){
-    next = curr->next;
-    free(curr->str);//fee data
-    free(curr);//free node
-    curr = next;
-  }
-  free(list);
-}
+void free_history(List *list);
 
 #endif
